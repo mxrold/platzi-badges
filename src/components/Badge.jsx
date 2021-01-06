c076988b7d8c551c75bebff988929b00d1c1783d
Martin Roldan <martinroldan889@gmail.com>
2021-01-06 16:20:30 -0300

    EStyles to the first component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
+
 
 class Badge extends React.Component {
     render () {
         return (
-            <div className="container">
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Conference Logotype"/>
                 </div>
 
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="" />
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="" />
                     <h1>Martin <br/> Roldan</h1>
                 </div>
 
-                <div className="">
-                    <p>FullStack Developer</p>
-                    <p>@martinrol</p>
+                <div className="Badge__section-info">
+                    <h3>FullStack Developer</h3>
+                    <span>@martinrol</span>
                 </div>
 
-                <div>
+                <div className="Badge__footer">
                     <p>#PlatziConf</p>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Badge from './components/Badge';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
 const container = document.getElementById('app');
 
