e9a641757eb12e6fc97a1b433f78015e00b65862
Martin Roldan <martinroldan889@gmail.com>
2021-01-18 21:01:32 -0300

    Implementación de un portal mediante ReactDOM sobre el boton delete

--- src/pages/BadgeDetails.jsx
+++ src/pages/BadgeDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import Badge from '../components/Badge';
@@ -47,7 +48,11 @@ export default function BadgeDetails (props) {
                             </div>
 
                             <div>
-                                <button className="btn btn-danger">Delete</button>
+                                <button  className="btn btn-danger">Delete</button>
+                                {ReactDOM.createPortal(
+                                <h1>Hola, realmente no estoy aquí</h1>,
+                                document.getElementById('modal')
+                                )}
                             </div>
                         </div>
                     </div>
